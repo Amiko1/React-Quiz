e445f98e02569ec73e4a05e64beef3f4f737da33
Amirani <amiraniburjanadze@gmail.com>
2023-10-29 15:38:21 +0400

    init application

---
+++ src/components/Options.js
@@ -0,0 +1,23 @@
+export default function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnser", payLoad: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/Progress.js
@@ -0,0 +1,18 @@
+export default function Progress({
+  index,
+  numQuestions,
+  points,
+  maxPossiblePoints,
+}) {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index} />
+      <p>
+        Question <strong>{index}</strong> / {numQuestions}
+      </p>
+      <p>
+        <strong>{points} </strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
